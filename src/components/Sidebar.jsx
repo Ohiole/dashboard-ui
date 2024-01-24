7274d00b655144ab481ecbe9a311f2c6ee418d80
Ohiole <eboreimeojo99@gmail.com>
2024-01-24 22:28:16 +0100

    Installing Vuesax icons and creating sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import Logo from '../assets/Logo.png'
+import { Category, TrendUp, Profile2User, Box, DiscountShape, InfoCircle, Moon, Sun1, Setting2, Logout, ArrowCircleRight2 } from 'iconsax-react'
 
 function Sidebar() {
   return (
-    <section>
-        
+    <section className=''>
+        <section>
+            <img src={Logo} alt='Company Logo' className='cursor-pointer w-[40px] h-[40px]'/>
+            <nav>
+                <ul>
+                    <li>
+                        <Category size="24" color="#0D062D" variant="Bulk"/>
+                        <div></div>
+                    </li>
+                    <li>
+                        <TrendUp size="24" color="#B2ABAB" variant="Broken"/>
+                        <div></div>
+                    </li>
+                    <li>
+                        <Profile2User size="24" color="#B2ABAB" variant="Broken"/>
+                        <div></div>
+                    </li>
+                    <li>
+                        <Box size="24" color="#B2ABAB" variant="Broken"/>
+                        <div></div>
+                    </li>
+                    <li>
+                        <DiscountShape size="24" color="#B2ABAB" variant="Broken"/>
+                        <div></div>
+                    </li>
+                    <li>
+                        <InfoCircle size="24" color="#B2ABAB" variant="Broken"/>
+                        <div></div>
+                    </li>
+                    <li>
+                        <Sun1 size="24" color="#B2ABAB" variant="Bold"/>
+                        <Moon size="24" color="#B2ABAB" variant="Bold"/>
+                    </li>
+                </ul>
+            </nav>
+        </section>
+        <section>
+            <nav>
+                <ul>
+                    <li>
+                        <ArrowCircleRight2 size="24" color="#B2ABAB" variant="Broken"/>
+                    </li>
+                    <li>
+                        <Setting2 size="24" color="#B2ABAB" variant="Broken"/>
+                    </li>
+                    <li>
+                        <Logout size="24" color="#B2ABAB" variant="Broken"/>
+                    </li>
+                </ul>
+            </nav>
+        </section>
     </section>
   )
 }
